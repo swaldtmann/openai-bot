ff8a0ed5149569a8d8ba7ed9c753a2310cc2fc06
Woo <stephan@waldtmann.de>
2023-01-15 13:15:58 +0100

    changed engine model

--- chatbot.py
+++ chatbot.py
@@ -12,6 +12,7 @@ completion = openai.Completion()
 start_chat_log = '''Human: Hello, who are you?
 AI: I am doing great. How can I help you today?
 '''
+engine_model = "text-davinci-003"
 
 
 def ask(question, chat_log=None):
@@ -19,7 +20,7 @@ def ask(question, chat_log=None):
         chat_log = start_chat_log
     prompt = f'{chat_log}Human: {question}\nAI:'
     response = completion.create(
-        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+        prompt=prompt, engine=engine_model, stop=['\nHuman'], temperature=0.9,
         top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
         max_tokens=150)
     answer = response.choices[0].text.strip()
