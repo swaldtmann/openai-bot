a83485f3488529b3125c93f9aceb48a9233ce1ec
Woo <stephan@waldtmann.de>
2023-01-17 13:05:42 +0100

    no its broken ...

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,19 +4,18 @@
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 import os
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
-from multiprocessing import Manager
 
 from dotenv import load_dotenv
 
-from chatbot import ask, append_interaction_to_chat_log
+from chatbot import ask, append_interaction_to_chat_log, chat_log_to_str
 
 load_dotenv()
-data = Manager().dict()
+data = dict()
 data['chat_log'] = None
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"],
@@ -25,21 +24,21 @@ app = App(token=os.environ["SLACK_BOT_TOKEN"],
 # Middleware
 @app.middleware  # or app.use(log_request)
 def log_request(logger, body, next):
-    logger.info(body)
+    logger.debug(body)
     return next()
 
 @app.event("message")
 def handle_message_events(event, say, logger):
     global data
-#    try:
     incoming_msg = event['text']
     answer = ask(incoming_msg, data['chat_log'])
-    data['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer, data['chat_log'])
-    logger.info(f"# of chars in chat_log: {len(data['chat_log'])}")
-    logger.info(data['chat_log'])
-    say(f"{answer}")
-#    except Exception as e:
-#        print(f'Error: {e}')
+    if answer:
+        data['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer, data['chat_log'])
+        logger.info(data['chat_log'])
+        logger.info(f"# of chars in chat_log: {len(chat_log_to_str(data['chat_log']))}")
+        say(f"{answer}")
+    else:
+        say(f"No answer from bot ...")
 
 # @app.event("message")
 # def handle_message():
@@ -56,7 +55,11 @@ def reset_command(ack, say):
 def reset_command(ack, say):
     global data
     ack()
-    say(f"Chat Log: \n{data['chat_log']}")
+    if data['chat_log']:
+        chat_log_str = chat_log_to_str(data['chat_log'])
+    else:
+        chat_log_str = "Chat log is empty."
+    say(f"Chat Log: \n{chat_log_str}")
 
 
 from flask import Flask, request
@@ -71,6 +74,7 @@ def slack_events():
 
 if __name__ == "__main__":
     flask_app.run(debug=True, 
+                  threaded=True,
                   host=os.getenv("FLASK_HOST", "127.0.0.1"),
                   port=os.getenv("FLASK_PORT", 3000))
 
--- flask_app/chatbot.py
+++ flask_app/chatbot.py
@@ -5,6 +5,12 @@ import os
 import logging
 from dotenv import load_dotenv
 import openai
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    retry_if_exception_type,
+    before
+)
 
 logger = logging.getLogger(__name__)
 
@@ -12,28 +18,63 @@ load_dotenv()
 openai.api_key = os.environ.get('OPENAI_KEY')
 completion = openai.Completion()
 
-start_chat_log = '''Human: Hello, who are you?
-AI: I am doing great. How can I help you today?
-'''
+start_chat_log = list()
+start_chat_log.append("Human: Hello, who are you?")
+start_chat_log.append("AI: I am doing great. How can I help you today?")
+
+chat_log = list()
+
 engine_model = "text-davinci-003"
 engine_max_tokens = 400
 
+def shorten_chat_log():
+    global chat_log
+    # TODO 
+    first_item = chat_log.pop(0)
+    second_item = chat_log.pop(0)
+    logger.info('shortened!')
+    return True
 
-def ask(question, chat_log=None):
-    try:
-        if chat_log is None:
-            chat_log = start_chat_log
-        prompt = f'{chat_log}Human: {question}\nAI:'
-        response = completion.create(
-            prompt=prompt, engine=engine_model, stop=['\nHuman'], temperature=0.9,
-            top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-            max_tokens=engine_max_tokens)
-        answer = response.choices[0].text.strip()
-        return answer
-    except Exception as e:
-        print(f'Error: {e}')    
+@retry(stop=stop_after_attempt(6),
+       before=shorten_chat_log(), 
+       retry=retry_if_exception_type(openai.error.Timeout))
+def ask_with_backoff(**kwargs):
+    return ask(**kwargs)
 
+def create_prompt(question, chat_log):
+    return f'{chat_log_to_str(chat_log)}\nHuman: {question}\nAI:'
+
+def ask(question, a_chat_log=None):
+    global chat_log
+    chat_log = a_chat_log
+    #try:
+    if chat_log is None:
+        chat_log = start_chat_log
+    prompt = create_prompt(question, chat_log)
+    logger.info(f'\nchat_log:\n{chat_log}')
+    logger.info(f'\nprompt:\n{prompt}')
+    response = completion.create(
+        prompt=prompt, engine=engine_model, stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=engine_max_tokens,
+        request_timeout=0.01, # exception debugging only!!
+        )
+    answer = response.choices[0].text.strip()
+    return answer
+
+"""     except openai.error.Timeout as exception:
+        print ("Timeout: {}".format(exception))
+
+    except Exception as exception:
+        print("Exception: {}".format(type(exception).__name__))
+        print("Exception message: {}".format(exception))
+ """    
 def append_interaction_to_chat_log(question, answer, chat_log=None):
     if chat_log is None:
         chat_log = start_chat_log
-    return f'{chat_log}Human: {question}\nAI: {answer}\n'
+    chat_log.append(f'Human: {question}')
+    chat_log.append(f'AI: {answer}')
+    return chat_log
+
+def chat_log_to_str(chat_log):
+    return "\n".join(chat_log)
